0e0ef8793d6cdb00484e1067eaa009ea64cb704e
GuoXiaoBing7 <562100739@qq.com>
2019-06-19 16:21:04 +0800

    Add custom helpers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+/*加载自定义的助手函数存放在bootstrap/helpers.php下*/
+require_once __DIR__.'/helpers.php';
 /*
 |--------------------------------------------------------------------------
 | Create The Application

720b2c116181276dcedfbb2530d3b51a8b348cf4
GuoXiaoBing7 <562100739@qq.com>
2019-06-21 16:15:01 +0800

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,12 @@ use Illuminate\Http\Request;
 use App\Http\Requests\UserRequest;
 class UsersController extends Controller
 {
-    //
+    //利用autho中间件进行身份验证，来过滤未登录用户可以修改其他人的信息
+    public function __construct()
+    {
+        $this->middleware('auth',['except'=>['show']]);
+    }
+
     public function show(User $user)
     {
         return view('users.show',compact('user'));
@@ -16,12 +21,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update',$user);
         return view('users.edit',compact('user'));
     }
 
     public function update(UserRequest $request,ImageUploadHander $uploader,User $user)
     {
-
+        $this->authorize('update',$user);
         $data = $request->all();
         if ($request->avatar){
             $result = $uploader->save($request->avatar,'avatars',$user->id,362);

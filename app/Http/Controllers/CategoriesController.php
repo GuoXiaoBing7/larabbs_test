049b25c7593dedd9b4c2c1f2f0b8b18b855d190f
GuoXiaoBing7 <562100739@qq.com>
2019-06-25 15:03:19 +0800

    List topics created by user

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -14,9 +14,10 @@ class CategoriesController extends Controller
     {
         //with() 预加载 避免N+1的问题
         //withorder()调用Topic.php 模型中的scopeWithOrder来匹配是最新回复的还是最新发表的
-       $topics = $topic->where('category_id',$category->id)
-                    ->withOrder($request->order)
-                    ->paginate(20);
+       $topics = $topic
+           ->where('category_id',$category->id)
+           ->withOrder($request->order)
+           ->paginate(20);
        return view('topics.index',compact('topics','category'));
     }
 }
